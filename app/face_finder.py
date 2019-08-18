8801d2cdf5ac8166da15d8133f992239959b6e4f
Joshua Jakowlew <jakowlew@pm.me>
2019-08-19 00:08:54 +0300

    Release 1.0.0 - Refactoring - Small changes in run.py - Added docs

--- app/face_finder.py
+++ app/face_finder.py
@@ -2,21 +2,38 @@ import face_recognition as fr
 import os
 
 class FaceFinder:
+    """This class allows you to find similar faces
+    based on training set.
+    """
     def __init__(self, train_path):
+        """Looks at images in training set
+        and calculates its face encodings
+
+        Args:
+            train_path: path to folder with training set
+        """
         self._known_encodings = []
 
         for image_name in os.listdir(train_path):
             image_name = f'{train_path}/{image_name}'
-            image = fr.load_image_file(image_name)
-            face_locations = fr.face_locations(
-                image, number_of_times_to_upsample=0, model="cnn")
-            image_encoding = fr.face_encodings(
-                image, known_face_locations=face_locations)
+            
+            image_encoding = self.get_encoding(image_name)
+
             if image_encoding: 
-                image_encoding = image_encoding[0]
+                image_encoding = image_encoding[0] # select first face
                 self._known_encodings.append(image_encoding)
 
     def find_face(self, image_path):
+        """Finds face in test image and compares them
+        to training set.
+        This method is a shortcut for
+        get_encoding + is_similar
+
+        Args:
+            image_path: Path to image in test set
+        Returns:
+            True if faces are similar, False if not
+        """
         encoding = self.get_encoding(image_path)
         if not encoding:
             return False
@@ -24,6 +41,15 @@ class FaceFinder:
         return self.is_similar(encoding)
     
     def get_encoding(self, image_path):
+        """Given an image, return the 128-dimension
+        face encoding for each face in the image.
+
+        Args:
+            image_path: Path to image in test set
+        Returns:
+            List of face encodings
+            Empty list if there are no faces found
+        """
         image = fr.load_image_file(image_path)
 
         face_locations = fr.face_locations(
@@ -35,6 +61,19 @@ class FaceFinder:
         return image_encoding
 
     def is_similar(self, test_image_encoding, treshold=0.6):
+        """Determines whether faces in
+        train_set and test image are similar
+
+        Args:
+            test_image_encoding: Encoding of test set image
+                (see get_encoding())
+            treshold: Faces with distance <= treshold are similar
+                0.6 is default and fits for most cases
+                try 0.5-0.55 for more strict results
+        Returns:
+            True if faces are similar, False if not
+        """
+
         distances = self.calculate_distance(test_image_encoding)
 
         if not distances:
@@ -45,8 +84,21 @@ class FaceFinder:
                 return True
         return False
 
-
     def calculate_distance(self, test_image_encoding):
+        """Calculates Euclidian distance between test set and given image
+
+        Distance is an average of distances to every image in train set.
+        If there are 3 images in test set and distances are
+        [0.7, 0.55, 0.6]
+        then resulting distance will be (0.7 + 0.55  +0.6) / 3 = 0.617
+
+        Args:
+            test_image_encoding: Encoding of test set image
+                (see get_encoding())
+        Returns:
+            Empty list if there are no encodings in test_image_encoding
+            List with average distances to every encoding in test_image_encoding
+        """
         if not test_image_encoding:
             return []
         
--- run.py
+++ run.py
@@ -3,24 +3,54 @@ from app.face_finder import FaceFinder
 import os
 import shutil
 
-# # dump images from VK
-# image_dumper = ImageDumper()
-# dumproot, _ = image_dumper.dump_images(id=448744461)
+# app version
+VERSION = '1.0.0'
 
-# encode training set
-face_finder = FaceFinder('train_images')
+def main():
+    # show name, version and license
+    greeting()
 
-dumproot = 'imagedump-2019-08-18-17-31-43'
+    # create directory for training set
+    # and ask user to put images in this folder
+    prompt_for_training_set()
 
-# create directory for similar faces
-if not os.path.exists('similar_images'):
-    os.mkdir('similar_images')
+    # encode training set
+    print('vk-findme > Training...')
+    face_finder = FaceFinder('train_images')
 
-for friend_dir in os.listdir(dumproot):
-    for image in os.listdir(f'{dumproot}/{friend_dir}'):
-        image_path = f'{dumproot}/{friend_dir}/{image}'
-        image_name = f'{friend_dir}-{image}'
-        res = face_finder.find_face(image_path)
-        if res:
-            print(f'Face found! ({image_name})')
-            shutil.copy2(image_path, f'similar_images/{image_name}')
+    # dump images from VK
+    print('vk-findme > Dumping images from VK...')
+    image_dumper = ImageDumper()
+    dumproot, _ = image_dumper.dump_images(id=448744461)
+
+    # create directory for similar faces
+    if not os.path.exists('similar_images'):
+        os.mkdir('similar_images')
+
+    print('vk-findme > Searching faces in test set...')
+    call_for_test_set(face_finder, dumproot)
+
+def greeting():
+    print(f'VK-Findme v.{VERSION}')
+    print('This software uses MIT license')
+
+def prompt_for_training_set():
+    if not os.path.exists('train_images'):
+        os.mkdir('train_images')
+        print('Put training images in "train_images" folder')
+        os.system('pause')
+
+def call_for_test_set(face_finder, dumproot):
+    for friend_dir in os.listdir(dumproot):
+        for image in os.listdir(f'{dumproot}/{friend_dir}'):
+            image_path = f'{dumproot}/{friend_dir}/{image}'
+            image_name = f'{friend_dir}-{image}'
+            find_face(face_finder, image_path, image_name)
+
+def find_face(face_finder, image_path, image_name):
+    if face_finder.find_face(image_path):
+        print(f'Face found! ({image_name})')
+        shutil.copy2(image_path, f'similar_images/{image_name}')
+
+if __name__ == '__main__':
+    main()
